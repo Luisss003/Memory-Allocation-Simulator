f113f2f71b9ec445c81cf5ff64988e0dafeec9b8
Luis Saenz <145069480+Luisss003@users.noreply.github.com>
2025-04-28 18:23:07 -0500

    Add files via upload

--- part3.c
+++ part3.c
@@ -40,15 +40,20 @@
  }
  
  int main(int argc, char *argv[]) {
-     if (argc != 6) {
+     
+    if (argc != 6) {
          printf("Usage: %s BytesPerPage SizeOfVirtualMemory SizeOfPhysicalMemory SequenceFile OutputFile\n", argv[0]);
          return 1;
      }
- 
+     int i;
      unsigned long BytesPerPage = atoi(argv[1]);
      unsigned long SizeOfVirtualMemory = atoi(argv[2]);
      unsigned long SizeOfPhysicalMemory = atoi(argv[3]);
- 
+     unsigned long LA, PA;
+     int current_time = 0;
+     int page_faults = 0;
+
+
      FILE *infile = fopen(argv[4], "rb");
      if (infile == NULL) {
          printf("Error opening input file");
@@ -90,45 +95,53 @@
      //Track last access time for LRU
      int *LRUcount = (int *)malloc(sizeof(int) * num_frames);
  
-     // Check for memory allocation errors
-     if (!PT || !freeframes || !revmap || !LRUcount) {
-         fprintf(stderr, "Memory allocation failed\n");
-         fclose(infile);
-         fclose(outfile);
-         free(PT);
-         free(freeframes);
-         free(revmap);
-         free(LRUcount);
-         return 1;
-     }
+// Check for memory allocation errors
+    if (PT == NULL || freeframes == NULL || revmap == NULL || LRUcount == NULL) {
+        printf("Memory allocation failed\n");
+        fclose(infile);
+        fclose(outfile);
+        free(PT);
+        free(freeframes);
+        free(revmap);
+        free(LRUcount);
+        return 1;
+    }
  
-     // Initialize tables
-     for (int i = 0; i < num_pages; i++) {
+     // Initializepage table entries
+     for (i = 0; i < num_pages; i++) {
          PT[i].valid_bit = 0;
          PT[i].frame_number = -1;
      }
+
+     //Initialize frame tracking arrays
      for (int i = 0; i < num_frames; i++) {
-         if (i == 0)
-             freeframes[i] = 0; // frame 0 reserved
-         else
+         if (i == 0){
+             freeframes[i] = 0; //frame 0 reserved
+         }
+         //Other frames are initially free 
+         else{
              freeframes[i] = 1;
-         revmap[i] = -1;
-         LRUcount[i] = 0;
+        }
+        //No pages mapped yet
+        revmap[i] = -1;
+
+        //No frames have been acecssed either
+        LRUcount[i] = 0;
      }
  
-     unsigned long LA, PA;
-     int current_time = 0;
-     int page_faults = 0;
- 
+     //While there is data to be read from the input file
      while (fread(&LA, sizeof(unsigned long), 1, infile) == 1) {
+         //Calculate page number and offset bits
          unsigned long pnum = LA >> d;
          unsigned long dnum = LA & offset_mask;
          int fnum;
  
+         //Check if page is valid
          if (PT[pnum].valid_bit == 1) {
              // Page is valid
              fnum = PT[pnum].frame_number;
-         } else {
+         } 
+         else { //Otherwise, page is not valid and needs to be loaded
              // Page fault
              page_faults++;
  
@@ -143,15 +156,17 @@
                  }
              }
  
-             if (!found_free) {
-                 // No free frames, use LRU replacement
+             //If there is no free frame found, use LRU replacement
+             if (found_free == 0) {
+                 
                  int victim_frame = select_victim_frame(LRUcount, num_frames);
  
+                // Invalidate the page currently using the victim frame
                  int victim_page = revmap[victim_frame];
                  if (victim_page != -1) {
                      PT[victim_page].valid_bit = 0;
                  }
- 
+                 // Assign the victim frame to the new page
                  fnum = victim_frame;
              }
  
@@ -161,14 +176,13 @@
              revmap[fnum] = pnum;
          }
  
-         // Update LRU counter
-         current_time++;
+        // Update the last-used time for the frame (for LRU tracking)         current_time++;
          LRUcount[fnum] = current_time;
  
          // Calculate physical address
          PA = (fnum << d) + dnum;
  
-         //
+         //Write physical addresses to the output file
          if (fwrite(&PA, sizeof(unsigned long), 1, outfile) != 1) {
              perror("Error writing to output file");
              fclose(infile);
